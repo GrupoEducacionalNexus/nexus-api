95d6c8cc05946390cb4a1da6c3ec9d4521f99684
nexus <grupoeducacionalnexus>
2024-10-04 16:05:49 -0300

    working / postman ssl error , prod connection closed

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,61 +1,43 @@
-// Importando os módulos necessários
 const express = require('express');
+const consign = require('consign');
 const cors = require('cors');
 const helmet = require('helmet');
-const rateLimit = require('express-rate-limit');
-const { readdirSync } = require('fs');
-const path = require('path');
 
-// FUNÇÃO RESPONSÁVEL POR CONFIGURAR O APP DO EXPRESS
+//FUNÇÃO RESPONSÁVEL POR CONFIGURAR O APP DO EXPRESS
 module.exports = () => {
   const app = express();
-
-  // Configurando o CORS para permitir apenas origens específicas
-  app.use(cors({
-    origin: ['https://www.gestorgruponexus.com.br', 'http://localhost:3000'],
-    credentials: true
-  }));
-
-  // Configuração de limite de tamanho de payload
-  app.use(express.urlencoded({ limit: '10mb', extended: false, parameterLimit: 10000 }));
-  app.use(express.json({ extended: false, limit: '10mb' }));
-
-  // Middleware de segurança - Helmet
+  // app.use(cors({ origin: ['https://www.gestorgruponexus.com.br', 'http://localhost:3000'], credentials: true }));
+  app.use(cors());
+  app.use(express.urlencoded({ limit: '50mb', extended: false, parameterLimit: 50000 }));
+  app.use(express.json({ extended: false, limit: '50mb' }));
+  // app.use(helmet({
+  //   referrerPolicy: { policy: 'strict-origin-when-cross-origin' }
+  // }));
   app.use(helmet.contentSecurityPolicy({
-    directives: {
-      "default-src": ["'self'"],
-      "script-src": ["'self'"],
-      "style-src": ["'self'"],
-      "img-src": ["'self'"],
-      "connect-src": ["'self'"],
-      "font-src": ["'self'"],
-      "object-src": ["'none'"],
-      "media-src": ["'self'"],
-      "frame-src": ["'none'"],
-      "base-uri": ["'self'"],
-      "form-action": ["'self'"],
-      "manifest-src": ["'self'"],
-      "frame-ancestors": ["'none'"],
-      "worker-src": ["'self'"],
-    }
-  }));
-
-  // Limitar requisições por IP para evitar ataques de DoS
-  const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutos
-    max: 100, // Limita cada IP a 100 requisições por janela de 15 minutos
-    message: 'Muitas requisições feitas deste IP, por favor tente novamente mais tarde.'
-  });
-  app.use(limiter);
-
-  // Carregar controladores manualmente
-  const controllersPath = path.join(__dirname, '../controllers');
-  readdirSync(controllersPath).forEach((file) => {
-    const controller = require(path.join(controllersPath, file));
-    if (typeof controller === 'function') {
-      controller(app);
-    }
-  });
-
+      directives: {
+        "default-src": ["'self'"],
+        "script-src": ["'self'"],
+        "style-src": ["'self'"],
+        "img-src": ["'self'"],
+        "connect-src": ["'self'"],
+        "font-src": ["'self'"],
+        "object-src": ["'none'"],
+        "media-src": ["'self'"],
+        "frame-src": ["'none'"],
+        "base-uri": ["'self'"],
+        "form-action": ["'self'"],
+        "manifest-src": ["'self'"],
+        "frame-ancestors": ["'none'"],
+        "worker-src": ["'self'"],
+        "upgrade-insecure-requests": [],
+        "block-all-mixed-content": [],
+        "plugin-types": [],
+        "sandbox": [],
+        "require-sri-for": [],
+        "report-uri": [],
+        "strict-origin-when-cross-origin": [] // add this directive
+      }
+    }));
+  consign().include('controllers').into(app);
   return app;
 };
